ea83ec368036c75e567d1fd659e0dd6efee1933c
Lukasz Klich <lukasz.klich@upc.pl>
2014-03-01 10:04:47 +0100

    I've pulled jdk to another recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,1 @@
-include_recipe "java::oracle"
-
-file "/etc/profile.d/jdk.sh" do
-  content "export JAVA_HOME=#{node['java']['java_home']}
-export PATH=#{node['java']['java_home']}/bin:$PATH"
-  mode 00755
-end
+include_recipe "dev-env::jdk"
