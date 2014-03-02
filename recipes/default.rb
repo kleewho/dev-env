f4d28dc86077a6c5a447526c1a3120e2c812fdc7
Lukasz Klich <lukasz.klich@upc.pl>
2014-03-02 22:20:45 +0100

    Still cleaning " '

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
-include_recipe "dev-env::jdk"
-include_recipe "dev-env::maven"
-include_recipe "dev-env::vagrant"
-include_recipe "dev-env::repos"
+include_recipe 'dev-env::jdk'
+include_recipe 'dev-env::maven'
+include_recipe 'dev-env::vagrant'
+include_recipe 'dev-env::repos'
+include_recipe 'dev-env::idea'
