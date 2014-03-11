fa1cece51cd91382eac8234ff18815b163cb6ebc
Lukasz Klich <lukasz.klich@upc.pl>
2014-03-11 23:38:41 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'klich.lukasz@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures whatever I need in my work'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 supports 'ubuntu'
 
